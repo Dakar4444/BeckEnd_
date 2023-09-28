ba801c9d5a69dc1c8629102b1709a3d943751992
Dakar4444 <77073529+Dakar4444@users.noreply.github.com>
2023-09-28 17:48:02 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 function send() {
-    //Ввод данных
-    //let x = prompt("Введите имя: ")
+
+    let name = document.getElementById('name').value;
     let message = document.getElementById('message').value;
 
-    //Логика
-    //x = x + 2
     if (message=='') {
         document.getElementById('error').innerText = "Введите сообщение!"
     }
-    else {
-        document.getElementById('result').innerText = message;
-        document.getElementById('error').innerText = ""
+        else {
+        document.getElementById('error').innerText = "";
+        document.getElementById('result').innerText = `\n ${name}: ${message}`;
     }
-
-    //Вывод данных
-    //alert(x)
-    //document.getElementById('result').value = message;
 }
